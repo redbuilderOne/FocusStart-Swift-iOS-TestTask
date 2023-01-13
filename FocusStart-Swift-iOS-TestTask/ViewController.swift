07fded2ff9a5c8647374892a21b7c31c22cda31b
redbuilderOne <forsaken2492@yandex.ru>
2023-01-13 14:15:59 +0300

    first commit

---
+++ FocusStart-Swift-iOS-TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FocusStart-Swift-iOS-TestTask
+//
+//  Created by Дмитрий Скворцов on 13.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
